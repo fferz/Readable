494fc498e560aa2a62e975a3dca580b3d66a0932
fer <fernandaz@live.com.ar>
2018-02-20 17:41:56 -0300

    Link added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { Route} from 'react-router-dom'
 import AddIcon from 'react-icons/lib/fa/plus-circle'
 import DownVoteIcon from 'react-icons/lib/fa/hand-o-down'
 import UpVoteIcon from 'react-icons/lib/fa/hand-o-up'
 import CommentsIcon from 'react-icons/lib/fa/comment-o'
-import AddPostIcon from 'react-icons/lib/fa/plus'
+import AddPostIcon from 'react-icons/lib/fa/plus-circle'
 import './App.css'
 
 class App extends Component {
@@ -45,48 +47,51 @@ class App extends Component {
                 </div>)}
         </div>
 
-        <div className="content">
-            <h1>Posts</h1>
-            {this.state.posts.map( post =>
-            <div key={post.id}> 
-              <div className="post-title">{post.title}</div>
-              <div className="post-author">{post.author}</div>
-              <div className="post-category">category:{post.category}</div> 
-              <div className="container">
-                  <div className="button-vote">
-                      <CommentsIcon size={20} />
-                      {post.commentCount}</div>
-                  <div className="button-vote">
-                    <button>
-                      <UpVoteIcon size={20} />
-                    </button>
-                  </div>
-                  <div className="button-vote">{post.voteScore}</div>
-                  <div className="button-vote">
-                    <button className="downVoteButton">
-                        <DownVoteIcon size={20} />
-                    </button>
-                  </div>
-                  <div className="button-vote">
-                    <button >edit</button>
-                  </div>
-                  <div className="button-vote">
-                    <button >delete</button>
-                  </div>
-              </div>
-              <div className="new-post">
-                 
-                  
-                      <a href="#" >
-                        <AddPostIcon size={50}  />
-                      </a>
-              
-              </div>
-          </div>)}
+        <Route exact path="/" render={() => (
+          <div className="content">
+              <h1>Posts</h1>
+              {this.state.posts.map( post =>
+              <div key={post.id}> 
+                <div className="post-title">{post.title}</div>
+                <div className="post-author">{post.author}</div>
+                <div className="post-category">category:{post.category}</div> 
+                <div className="container">
+                    <div className="button-vote">
+                        <CommentsIcon size={20} />
+                        {post.commentCount}</div>
+                    <div className="button-vote">
+                      <button>
+                        <UpVoteIcon size={20} />
+                      </button>
+                    </div>
+                    <div className="button-vote">{post.voteScore}</div>
+                    <div className="button-vote">
+                      <button className="downVoteButton">
+                          <DownVoteIcon size={20} />
+                      </button>
+                    </div>
+                    <div className="button-vote">
+                      <button >edit</button>
+                    </div>
+                    <div className="button-vote">
+                      <button >delete</button>
+                    </div>
+                </div>
+                <div className="new-post">
+                  <Link
+                      to="/create"
+                      className="add-post"
+                    ><AddPostIcon size={50} />
+                      
+                  </Link>
+                </div>
+            </div>)}
+          </div>
+        )} />
 
-
-        </div>
         
+
+
       </div>
     );
   }
