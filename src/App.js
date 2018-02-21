0e05f11f49049be2dccd4a17d28f657fa26aaf9d
fer <fernandaz@live.com.ar>
2018-02-20 21:36:59 -0300

    Form added: html + css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import DownVoteIcon from 'react-icons/lib/fa/hand-o-down'
 import UpVoteIcon from 'react-icons/lib/fa/hand-o-up'
 import CommentsIcon from 'react-icons/lib/fa/comment-o'
 import AddPostIcon from 'react-icons/lib/fa/plus-circle'
+import NewPost from './NewPost.js'
 import './App.css'
 
 class App extends Component {
@@ -37,7 +38,7 @@ class App extends Component {
   render() {
     var i =1
     return (
-      <div className="App">
+      <div className="app-container">
         
         <div className="sidenav">
             {this.state.categories.map( category =>
@@ -47,47 +48,59 @@ class App extends Component {
                 </div>)}
         </div>
 
-        <Route exact path="/" render={() => (
           <div className="content">
-              <h1>Posts</h1>
-              {this.state.posts.map( post =>
-              <div key={post.id}> 
-                <div className="post-title">{post.title}</div>
-                <div className="post-author">{post.author}</div>
-                <div className="post-category">category:{post.category}</div> 
-                <div className="container">
-                    <div className="button-vote">
-                        <CommentsIcon size={20} />
-                        {post.commentCount}</div>
-                    <div className="button-vote">
-                      <button>
-                        <UpVoteIcon size={20} />
-                      </button>
+            
+            <Route exact path="/" render={() => (
+              <div>
+                <h1>Posts</h1>
+                {this.state.posts.map( post =>
+                  <div key={post.id}> 
+                    <div className="post-title">{post.title}</div>
+                    <div className="post-author">{post.author}</div>
+                    <div className="post-category">category:{post.category}</div> 
+                    <div className="post-container">
+                        <div className="button-vote">
+                            <CommentsIcon size={20} />
+                            {post.commentCount}</div>
+                        <div className="button-vote">
+                          <button>
+                            <UpVoteIcon size={20} />
+                          </button>
+                        </div>
+                        <div className="button-vote">{post.voteScore}</div>
+                        <div className="button-vote">
+                          <button className="downVoteButton">
+                              <DownVoteIcon size={20} />
+                          </button>
+                        </div>
+                        <div className="button-vote">
+                          <button >edit</button>
+                        </div>
+                        <div className="button-vote">
+                          <button >delete</button>
+                        </div>
                     </div>
-                    <div className="button-vote">{post.voteScore}</div>
-                    <div className="button-vote">
-                      <button className="downVoteButton">
-                          <DownVoteIcon size={20} />
-                      </button>
+                    <div className="new-post">
+                      <Link
+                          to="/create"
+                          className="add-post"
+                        ><AddPostIcon size={50} />
+                          
+                      </Link>
                     </div>
-                    <div className="button-vote">
-                      <button >edit</button>
-                    </div>
-                    <div className="button-vote">
-                      <button >delete</button>
-                    </div>
-                </div>
-                <div className="new-post">
-                  <Link
-                      to="/create"
-                      className="add-post"
-                    ><AddPostIcon size={50} />
-                      
-                  </Link>
-                </div>
-            </div>)}
+                </div>)}
+              </div>
+            )}/>
+            
+
+            <Route path="/create" render={() => (
+                <NewPost
+                  categories={this.state.categories} />
+            )}/>
+
           </div>
-        )} />
+        
+        
 
         
 
